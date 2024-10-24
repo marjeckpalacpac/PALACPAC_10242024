e51d6b0a1eda9b912f01cf329dfcd7093ecdaec3
Marjeck Palacpac <marjeck.palacpac@ogglobal.net>
2024-10-24 11:32:40 +0800

    File upload validate file extension

---
+++ API/Controllers/FileProcessingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileProcessingController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Upload([FromForm] IFormFile? file)
+        {
+
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest(new ProblemDetails { Title = "Invalid file" });
+            }
+
+            var extension = Path.GetExtension(file.FileName).ToLower();
+
+            if (extension == ".csv")
+            {
+                return Ok("csv");
+            }
+            else if (extension == ".json")
+            {
+                return Ok("json");
+            }
+            else
+            {
+                return BadRequest(new ProblemDetails { Title = "Unsupported file format" });
+            }
+
+        }
+    }
+}
