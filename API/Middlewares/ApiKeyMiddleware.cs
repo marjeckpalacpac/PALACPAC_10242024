13b7c13154d1b57b4028adab2fd2f5f82b756d27
Marjeck Palacpac <marjeck.palacpac@ogglobal.net>
2024-10-24 13:39:28 +0800

    Secure API thru middleware. Setup api key in appsettings

---
+++ API/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace API.Middlewares
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string APIKEY_HEADER_NAME = "Api-Key";
+        private readonly string _apiKey;
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _apiKey = configuration.GetValue<string>("ApiKeySettings:ApiKey");
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY_HEADER_NAME, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401; 
+                await context.Response.WriteAsync("API Key is missing.");
+                return;
+            }
+
+            if (!_apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Invalid API Key.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
